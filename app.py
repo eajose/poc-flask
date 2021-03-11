e0e0cd53d48a6e4585afeb3205cc51b22defac9b
José Eduardo Albuquerque <jose.albuquerque@synergiaconsultoria.com.br>
2021-03-11 18:23:32 -0300

    Cria template base para reciclar os estilos dos templates.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# compose_flask/app.py
+
 from flask import Flask, render_template
 from redis import Redis
 
@@ -7,14 +7,24 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 
-@app.route('/')
-def hello():
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+
+
+@app.route('/index')
+def index():
     redis.incr('hits')
-    return render_template('indicadores.html')
+    return render_template('dashboard.html')
     # return (
     #     'This repository was created to practice development of microservices with Flask and this page has been viewed'
     #     ' %s time(s).' % redis.get('hits')
     #     )
+
+
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
 
 
 if __name__ == "__main__":
